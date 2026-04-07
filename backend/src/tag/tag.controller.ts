876f794d2b5bb80d309b3760ffa81e3d61097996
TAKOS <>
2026-04-07 18:22:25 +0200

    Backend : IA tags suggestions

--- backend/src/tag/tag.controller.ts
+++ backend/src/tag/tag.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, FileTypeValidator, Get, MaxFileSizeValidator, ParseFilePipe, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('tag')
 export class TagController {
@@ -19,5 +20,24 @@ export class TagController {
     @Get('suggest')
     suggestTag(@Query('tag') tag: string): Promise<string[]> {
         return this.tagService.suggestTag(tag);
+    }
+
+
+
+    @UseGuards(AuthGuard)
+    @Get('/ia-suggestions')
+    @UseInterceptors(FileInterceptor('post'))
+    iaSuggestions(@UploadedFile(
+        new ParseFilePipe({
+            validators: [
+                new MaxFileSizeValidator({ maxSize: 5 * 1000 * 1000 }),
+                new FileTypeValidator({
+                    fileType: /^image\/(png|jpe?g)(;.*)?$/i,
+                    fallbackToMimetype: true,
+                }),
+            ],
+        }),
+    ) post: Express.Multer.File): Promise<string[]> {
+        return this.tagService.iaSuggestions(post);
     }
 }
--- backend/src/tag/tag.service.ts
+++ backend/src/tag/tag.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { GenerateContentResponse, GoogleGenAI } from "@google/genai";
+import { $Enums } from '@prisma/client';
 
 @Injectable()
 export class TagService {
+    private readonly gemini: GoogleGenAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    private readonly model: string = 'gemini-2.5-flash-lite';
+
     constructor(private readonly prisma: PrismaService) {}
 
 
@@ -28,5 +33,34 @@ export class TagService {
         });
 
         return tags.map(t => t.name);
+    }
+
+
+
+    async iaSuggestions(post: Express.Multer.File): Promise<string[]> {
+        try {
+            const response = await this.gemini.models.generateContent({
+                model: this.model,
+                contents: [
+                    { text: 'Analyze this image and generate 3 descriptive tags for Instagram. Return ONLY the tags separated by commas. Do not use hashtags, brackets, or any preamble. Example: paris, france, baguette' }, 
+                    { inlineData: {
+                        mimeType: post.mimetype,
+                        data: post.buffer.toString('base64')
+                    }}
+                ]
+            });
+
+            if (!response.text) {
+                throw new InternalServerErrorException('Internal server error');
+            }
+
+            return response.text
+                .replace(/[\[\]"'`]/g, '')
+                .split(',')
+                .map(tag => tag.trim())
+                .filter(tag => tag.length > 0);
+        } catch (error) {
+            throw new InternalServerErrorException('Internal server error');
+        }
     }
 }
